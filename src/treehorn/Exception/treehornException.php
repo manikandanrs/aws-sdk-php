483566cd79106c26538468eb29e3d645b4743768
Manikandan Subramanian <subramam@amazon.com>
2017-01-10 13:19:29 -0800

    Adding treehorn service

---
+++ src/treehorn/Exception/treehornException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Aws\treehorn\Exception;
+
+use Aws\Exception\AwsException;
+
+/**
+ * Represents an error interacting with the **Amazon Elastic Treehorn** service.
+ */
+class treehornException extends AwsException {}
---
+++ src/treehorn/treehornClient.php
@@ -0,0 +1,9 @@
+<?php
+namespace Aws\treehorn;
+
+use Aws\AwsClient;
+
+/**
+ * This client is used to interact with the **Amazon Elastic Treehorn** service.
+ */
+class treehornClient extends AwsClient {}
